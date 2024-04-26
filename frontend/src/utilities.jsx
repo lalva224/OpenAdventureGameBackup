bd00d5e7b6ebfb3b504d81ca8a1bc844811955ff
Leandro Alvarez <alvarezleandro34363@gmail.com>
2024-04-26 16:32:03 -0400

    fixed base url

--- frontend/src/utilities.jsx
+++ frontend/src/utilities.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios'
 
 export const api = axios.create({
-baseURL :'http://ec2-18-212-35-86.compute-1.amazonaws.com:8000/api/v1/adventuregame/',
+baseURL :'https://openadventure.duckdns.org:8000/api/v1/adventuregame/',
+
 })
