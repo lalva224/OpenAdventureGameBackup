684aef4a1271ed6262635e946f7bbf3c96f4abd5
Leandro Alvarez <alvarezleandro34363@gmail.com>
2024-04-26 17:30:33 -0400

    updated settings

--- adventure_proj/adventure_proj/settings.py
+++ adventure_proj/adventure_proj/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["https://openadventure.duckdns.org/"]
 CSRF_TRUSTED_ORIGINS = ['https://openadventure.duckdns.org/']
 
 # Application definition
